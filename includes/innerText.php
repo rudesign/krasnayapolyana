add8e9d2fe322354dae984c8ba6fd6933154cced
Alex <220619@gmail.com>
2014-10-22 14:05:34 +0400

    services forms workout

--- includes/innerText.php
+++ includes/innerText.php
@@ -1,7 +1,7 @@
 <?php class_exists('Core', false) or die();
 
-if(!empty(Core::$item['teaser'])) echo '<div class="mini-section">'.Templates::parse(decodeHTMLEntities(Core::$item['teaser']), true).'</div>';
-if(!empty(Core::$item['body'])) echo '<div class="text mini-section">'.Templates::parse(decodeHTMLEntities(Core::$item['body']), true).'</div>';
+if(!empty(Core::$item['teaser'])) echo '<div class="section">'.Templates::parse(decodeHTMLEntities(Core::$item['teaser']), true).'</div>';
+if(!empty(Core::$item['body'])) echo '<div class="text section">'.Templates::parse(decodeHTMLEntities(Core::$item['body']), true).'</div>';
 
 
 ?>
